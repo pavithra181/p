fcba48c448caadc041c21f19b67de1d64a75faaf
pavithra181 <47211135+pavithra181@users.noreply.github.com>
2019-02-04 15:51:13 +0530

    Create greatest.py

---
+++ greatest.py
@@ -0,0 +1,9 @@
+n1=int(input())
+n2=int(input())
+n3=int(input())
+if n1>n2 and n1>n3:
+ print(n1,"n1 is the greatest")
+elif n2>n3:
+ print(n2,"n2 is the greatest")
+else:
+ print(n3,"n3 is the greatest")
